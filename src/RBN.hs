8e8b570996b4f02ac90353cef9567a315be590ba
Ali Nisar Ahmed <ali.nisar87@gmail.com>
2021-06-18 10:28:33 -0600

    add hspec-megaparsec; try parsing double colons in title

--- src/RBN.hs
+++ src/RBN.hs
@@ -8,6 +8,8 @@ import RIO
 import Text.Megaparsec (Parsec)
 import qualified Text.Megaparsec as MP
 import qualified Text.Megaparsec.Char as MPC
+import qualified Text.Megaparsec.Char.Lexer as MPCL
+import Text.Megaparsec.Debug (dbg)
 
 type Parser = Parsec Void Text
 
@@ -23,22 +25,20 @@ articleInfoParser :: Parser ArticleInfo
 articleInfoParser = do
   MPC.char 'T'
   MPC.space1
-  (title, author) <- parseWithoutTitle <|> parseWithTitle
+  (title, author) <- MP.try parseWithoutTitle <|> parseWithTitle
   pure $ ArticleInfo title author
 
 parseWithoutTitle :: Parser (Maybe Text, Text)
 parseWithoutTitle = do
-  MPC.char ':'
-  author <- MP.manyTill (MP.satisfy (/= '\n')) MPC.newline
+  dbg "parse colon" $ MPC.char ':'
+  author <- dbg "many till author" $ MP.manyTill (MP.satisfy (/= '\n')) MPC.newline
   pure (Nothing, T.pack author)
-
 
 parseWithTitle :: Parser (Maybe Text, Text)
 parseWithTitle = do
   title <- MP.manyTill (MP.satisfy (/= ':')) (MPC.char ':')
   author <- MP.manyTill (MP.satisfy (/= '\n')) MPC.newline
-  pure (Just (T.pack title), T.pack author)
-
+  pure (Just $ T.pack title, T.pack author)
 
 ---- Date Parser ----
 
@@ -58,5 +58,27 @@ parseYYYYMM :: String -> Maybe Day
 parseYYYYMM = parseTimeM True defaultTimeLocale "%Y%m"
 
 --- 1999
+
 parseYYYY :: String -> Maybe Day
 parseYYYY = parseTimeM True defaultTimeLocale "%Y"
+
+
+
+-- parseWithTitle :: Parser (Maybe Text, Text)
+-- parseWithTitle = do
+--   title <- dbg "manytill title" $ MP.manyTill (MP.try nonColon <|> doubleColon) (MPC.char ':')
+--   author <- MP.manyTill (MP.satisfy (/= '\n')) MPC.newline
+--   pure (Just $ T.pack title, T.pack author)
+--   where
+--     nonColon = MP.satisfy (/= ':')
+--     doubleColon = do
+--       MP.notFollowedBy (MPC.char ':')
+--       c1 <- MPC.char ':'
+--       c2 <- MPC.char ':'
+--       pure c1
+
+-- doubleColonParser :: Parser Char
+-- doubleColonParser = do
+--   dbg "not followed by" $ MP.notFollowedBy (MPC.char ':')
+--   c <- dbg "parsing first colon" $ (MP.satisfy (/= ':'))
+--   pure c
--- test/Spec.hs
+++ test/Spec.hs
@@ -4,6 +4,7 @@ import RIO
 import Test.Hspec
 import RBN
 import qualified Text.Megaparsec as MP
+import Test.Hspec.Megaparsec
 import Data.Time
 
 main :: IO ()
@@ -20,21 +21,26 @@ main = hspec do
       testBothTitleAndAuthor
     it "should successfully parse only author" do
       testAuthorOnly
+    -- it "should successfully parse title that contains an escaped ':'"
+    --   testTitleWithColon
 
 testAuthorOnly :: IO ()
 testAuthorOnly = do
   let input = "T :Ali Ahmed\n"
-  let Right (ArticleInfo title author) = MP.parse articleInfoParser "" input
-  title `shouldSatisfy` isNothing
-  author `shouldBe` "Ali Ahmed"
+  MP.parse articleInfoParser "" input `shouldParse` ArticleInfo Nothing "Ali Ahmed"
 
 testBothTitleAndAuthor :: IO ()
 testBothTitleAndAuthor = do
   let input = "T This is my world:Ali Ahmed\n"
-  let Right (ArticleInfo title author) = MP.parse articleInfoParser "" input
-  title `shouldSatisfy` isJust
-  title `shouldBe` (Just "This is my world")
-  author `shouldBe` "Ali Ahmed"
+  MP.parse articleInfoParser "" input `shouldParse` ArticleInfo (Just "This is my world") "Ali Ahmed"
+
+-- testTitleWithColon :: IO ()
+-- testTitleWithColon = do
+--   let input = "T this title ::has colons:Ali Ahmed"
+--   let Right (ArticleInfo title author) = MP.parse articleInfoParser "" input
+--   title `shouldSatisfy` isJust
+--   title `shouldBe` (Just "this title ::has colons")
+--   author `shouldBe` "Ali Ahmed"
 
 testYYYYMMDD :: IO ()
 testYYYYMMDD = do
